66c018112c4011e490b1b209d1afba3cf94e74a0
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-15 16:25:50 +0530

    create a header component for the restaurant app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+    <div className="header">
+        <div className='logo-container'>
+          <img className="logo" src = "https://static.vecteezy.com/system/resources/previews/019/607/567/non_2x/fast-food-vector-clipart-design-graphic-clipart-design-free-png.png"></img>
+        </div>
+        <div className='nav-container'>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+  )
+};
+
+const Body = () => {
+  return (
+    <div className="header">
+        <div className='logo-container'>
+          <img className="logo" src = "https://static.vecteezy.com/system/resources/previews/019/607/567/non_2x/fast-food-vector-clipart-design-graphic-clipart-design-free-png.png"></img>
+        </div>
+        <div className='nav-container'>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+  )
+};
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/>
   );
 }
 
