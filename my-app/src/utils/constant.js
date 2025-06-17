46a7ed8830e4946c663f62689ae57d96a9171cd3
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-17 16:05:07 +0530

    added all the constants link to the separate file

---
+++ my-app/src/utils/constant.js
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =  "https://static.vecteezy.com/system/resources/previews/019/607/567/non_2x/fast-food-vector-clipart-design-graphic-clipart-design-free-png.png";
