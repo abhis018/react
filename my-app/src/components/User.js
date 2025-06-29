2f64999dc7564b15513b714d0ad7cedd4f91b3d4
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-29 22:22:46 +0530

    created a user component

---
+++ my-app/src/components/User.js
@@ -0,0 +1,8 @@
+const User = () => {
+    return <div className="user-card">
+        <h2>name: Abhishek</h2>
+        <h2>location : Dumka</h2>
+        <h2>contact: Abhikr71</h2>
+    </div>
+}
+export default User;
