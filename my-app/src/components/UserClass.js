f7dcf92093d0761291ca8b1a7f94c111a16504e6
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-30 20:08:41 +0530

    set the count value

--- my-app/src/components/UserClass.js
+++ my-app/src/components/UserClass.js
@@ -3,12 +3,24 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            count: 0,
+        }
     }
     render(){
         const {name} = this.props;
+        const {count} = this.state;
 
         return(
         <div className="user-card">
+            <h1>count : {count}</h1>
+            <button onClick={() => {
+                this.state({
+                    count: this.state.count + 1;
+                })
+            }}>
+                Update Count
+            </button>
             <h2>name: {name}</h2>
             <h2>location : Dumka</h2>
             <h2>contact: Abhikr71</h2>
