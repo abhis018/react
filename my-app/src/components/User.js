0a1d0b337ae1f79a8ffa0e278ea6096cec8cc5ce
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-30 20:09:04 +0530

    update the count value using setCount

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const User = ({name}) => {
-    const [count] = useState(0);
+    const [count, setCount] = useState(0);
 
     return (
     <div className="user-card">
