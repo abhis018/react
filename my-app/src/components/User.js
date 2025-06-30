51326c1c64cc2a879d183344a8dedc77d19cbf4f
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-30 19:03:54 +0530

    change the variable through props

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,8 +1,10 @@
-const User = () => {
-    return <div className="user-card">
-        <h2>name: Abhishek</h2>
+const User = ({name}) => {
+    return (
+    <div className="user-card">
+        <h2>name: {name}</h2>
         <h2>location : Dumka</h2>
         <h2>contact: Abhikr71</h2>
     </div>
+    )
 }
 export default User;
