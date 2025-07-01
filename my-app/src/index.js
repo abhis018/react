eaec68ae3b7613690ac7799712d199e562ed73e7
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-01 20:28:37 +0530

    routing the about page from react router dom

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About';
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+  },
+  {
+    path:"/about",
+    element: <About/>
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
