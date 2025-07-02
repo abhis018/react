be7300713990a9ed0880a588ab6490ab048aed84
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-02 20:30:12 +0530

    adding a messsage when there is no internet connection

--- my-app/src/components/Body.js
+++ my-app/src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from './RestaurantCard';
 import { useState, useEffect } from 'react';
 import Shimmer from './Shimmer';
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 const Body = () => {
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -19,6 +20,15 @@ const Body = () => {
     setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
+
+    const onlineStatus = useOnlineStatus();
+
+    if(onlineStatus === false) 
+        return(
+            <h1>
+                Looks like you're offline !! Please check your internet connection;
+            </h1>
+        )
 
   return listOfRestaurants.length === 0 ? (<Shimmer/> ): (
     <div className="body">
