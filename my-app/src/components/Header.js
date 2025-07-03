9920f076dd995e7609f365a6ce6dd8198b738012
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-03 16:57:57 +0530

    added grocery list in my UI

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -17,6 +17,7 @@ const Header = () => {
           <ul>
             <li>Online Status: {onlineStatus ?"🟢" :"🔴"}</li>
             <li><Link to ="/">Home</Link></li>
+            <li><Link to="/grocery">Grocery</Link></li>
             <li><Link to ="/about">About Us</Link></li>
             <li><Link to ="/contact">Contact Us</Link></li>
             <li><Link to ="/cart">Cart</Link></li>
